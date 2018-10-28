5b722bed5ee73ad051b0a9d70a6ee3bca963686c
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-10-28 17:32:19 -0400

    made changes to routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,36 +13,35 @@ module.exports = function (app) {
 	app.get("/api/friends", function (req, res) {
 		res.json(friends);
     });
-    
-app.post("/api/friends", function(req, res){
+ app.post("/api/friends", function(req, res){
     //grabs the new users scores to compare with friends in the friends array
     const newFriendScore = req.body.scores;
     const scoresArray = [];
-    const friendCount = 0;
-    const bestMatch = 0;
+    var friendCount = 0;
+    var bestMatch = 0;
 
     //loop to run through the current friends list
-    for (const i=0; i<friends.length; i++){
-        const totalDifference = 0;
+    for (var i=0; i<friends.length; i++){
+        var totalDifference = 0;
         //run through the scores in the friends array to compare friends
-        for (const f=0; f<newFriendScore.length, f++;) {
+        for (var j=0; j<newFriendScore.length, j++){            
             totalDifference +- (Math.abs(parseInt(friends[i].scores[j])- parseInt(newFriendScore[j])));
         }
         //push into the scoresArray
         scoresArray.push(totalDifference);
     }
     // after all friends are compared find the best match based on similar scores
-    for (const i=0; i<scoresArray.length; i++){
+    for (var i=0; i<scoresArray.length; i++){
         if(scoresArray[i]<= scoresArray[bestMatch]){
             bestMatch = i;
         }
     }
     //retun bestMatch data
     const veryBestFriend = friends[bestMatch];
-    res.json(veryBestFriend);
-
     //push new submission into the friends array
+
     friends.push(req.body);
+    res.json(veryBestFriend);
 
 });
 
