ccfb952e2bdebbf6cd93128d996c531e279f531d
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-10-28 16:21:33 -0400

    created new questions for survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 const users = [
     {
-        name: "Lauren S",
-        photo: "https://memeguy.com/photos/images/i-think-i-found-my-favorite-stock-photo-129777.jpg",
+        name: "",
+        photo: "",
         scores:[
             5,
             5,
@@ -17,8 +17,8 @@ const users = [
     },
 
     {
-        name: "Mark P",
-        photo: "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-16-5a392518213b6__700.jpg",
+        name: "",
+        photo: "",
         scores: [
             4,
             5,
@@ -34,8 +34,8 @@ const users = [
     },
 
     {
-        name: "Carl Coffee",
-        photo: "https://st2.depositphotos.com/2631505/5462/i/950/depositphotos_54626107-stock-photo-happy-businessman-holding-funny-huge.jpg",
+        name: "",
+        photo: "",
         scores: [
             3,
             5,
@@ -51,8 +51,8 @@ const users = [
     },
 
     {
-        name: "Mark Hamilltime",
-        photo: "https://thumbs.dreamstime.com/z/funny-crazy-guy-hammer-10882175.jpg",
+        name: "",
+        photo: "",
         scores: [
             5,
             2,
@@ -68,8 +68,8 @@ const users = [
     },
 
     {
-        name: "Dennis Hopper",
-        photo: "http://l7.alamy.com/zooms/2416caa4779a463f87a1154ede8288d7/smiling-man-wearing-bunny-ears-and-holding-a-rabbit-cmnnnr.jpg",
+        name: "",
+        photo: "",
         scores: [
             3,
             4,
